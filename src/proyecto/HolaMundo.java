0e9bc96fbd27a56650b97bfb62f17baa8c1d7215
Julio Dominguez <chiapasjeremai@gmail.com>
2022-06-11 20:47:20 -0500

    Creando estructuura del proyecto

---
+++ src/proyecto/HolaMundo.java
@@ -0,0 +1,26 @@
+package proyecto;
+
+
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        int numero = 9;
+        long numero2 = 823;
+        byte data = 34;
+        short cantidad = 23;
+        boolean isNun = false;
+        String saludo = "Hola bienvenido al OpenBootcamp";
+        char consonante = 'a';
+        float precio = 65.f;
+        double calificaion = 9.9d;
+        System.out.println("Numero: "+numero);
+        System.out.println("Numero dos: "+numero2);
+        System.out.println("Data: "+data);
+        System.out.println("Cantidad: "+cantidad);
+        System.out.println("isnum: "+isNun);
+        System.out.println("Saludo: "+saludo);
+        System.out.println("Consonante: "+consonante);
+        System.out.println("Precio: "+precio);
+        System.out.println("Calificacion: "+calificaion);
+    }
+}
